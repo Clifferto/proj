d481618220e046c8bf2c620115174df5a81fdd51
Clifferto <ferrarisdomingo17@gmail.com>
2021-02-03 12:29:38 -0300

    actualizacion

---
+++ microBase/microBase.vitis/microBase/Debug/src/xgpio_tapp_example.d
@@ -0,0 +1,44 @@
+src/xgpio_tapp_example.o src/xgpio_tapp_example.o: \
+ ../src/xgpio_tapp_example.c \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xgpio.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_assert.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xstatus.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xgpio_l.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_io.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_printf.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/bspconfig.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/mb_interface.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_exception.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xstatus.h \
+ /media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_printf.h
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xgpio.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_assert.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xstatus.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xgpio_l.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_io.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/bspconfig.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/mb_interface.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_exception.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xstatus.h:
+
+/media/cliff/procom/verilog/microBase/microBase.vitis/hwBlk_wrapper/export/hwBlk_wrapper/sw/hwBlk_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
